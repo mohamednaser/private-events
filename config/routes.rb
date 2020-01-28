05e580555014f2fb867834c2db871b99751a5efa
Mohamed Naser <nc_m.naser@hotmail.com>
2020-01-28 22:31:07 +0200

    create signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get "/signup", to: "user#new"
+  post "/signup", to: "user#create"
+
+  get 'user/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

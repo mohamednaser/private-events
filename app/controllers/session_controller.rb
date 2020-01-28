70cb21b1ac089635368ed70ec56a98fc9be54146
Mohamed Naser <nc_m.naser@hotmail.com>
2020-01-28 22:45:54 +0200

    add login routes

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,10 @@
+class SessionController < ApplicationController
+  def create
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  post 'session/create'
+  get '/login', to: 'session/new'
+  post '/logout' , to: 'session/destroy'
+
   get "/signup", to: "user#new"
   post "/signup", to: "user#create"
+
+  post "/signin", to: "user#create"
 
   get 'user/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

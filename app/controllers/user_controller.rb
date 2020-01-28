3a76fb8c1a36cc7da5583c0e265596d9d2a4509c
Mohamed Naser <nc_m.naser@hotmail.com>
2020-01-28 22:42:00 +0200

    finish signup action

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,21 +1,21 @@
+# frozen_string_literal: true
+
 class UserController < ApplicationController
-  # will be post method  
+  # will be post method
   def create
     User.create(user_params)
+    
   end
 
-    
   def new
     @user = User.new
   end
 
-  def show
-  end
- 
-  private 
+  def show; end
 
-  def user_params     
-    debugger
-    params.require(:signup).permit(:name , :email)
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end

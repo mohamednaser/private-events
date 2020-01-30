5be8e301851bd1ab97871e2e209f9d207de00a79
Mohamed Naser <nc_m.naser@hotmail.com>
2020-01-30 12:31:58 +0200

    redirect to all events after create

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,15 +1,34 @@
+# frozen_string_literal: true
+
 class EventController < ApplicationController
+
+  before_action :check_login 
+
+  # get url
   def new
+    @event = Event.new
   end
 
+  # we will used it for post parmaters
   def create
+    @curent_user.events.create(event_params)
+    redirect_to events_path
   end
 
   def index
     @events = Event.all
-  
   end
 
-  def destroy
+  def destroy; end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description)
+  end
+
+  def check_login
+    curent_user
+    redirect_to login_path if !is_logged?
   end
 end
